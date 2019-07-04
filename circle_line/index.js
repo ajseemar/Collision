26deb2b9e71aa6ba6b260b426072315a07a438f9
aseemar <ajseemar@gmail.com>
2019-07-04 13:09:34 -0700

    create method that finds the intersection point between two lines

--- circle_line/index.js
+++ circle_line/index.js
@@ -1,12 +1,60 @@
 var c, ctx;
 document.addEventListener("DOMContentLoaded", () => {
+    let initialTime = Date.now();
+
     c = document.getElementById('canvas');
     ctx = c.getContext('2d');
 
-    w = new Wall(new Point(0, c.height - (c.height / 4)), new Point(c.width, c.height - (c.height / 4)));
-    e = new Wall(new Point(0, 0), new Point(5, 5));
-    w.render();
+    const w = new Wall(new Point(0, c.height - (c.height / 4)), new Point(c.width, c.height - (c.height / 4)));
+    const e = new Wall(new Point(0, 0), new Point(c.width, c.height));
+    const p = new Circle(new Point(10, c.height / 2), 5);
+    const o = new Circle(new Point(c.width / 2, c.height / 4), 5);
+
+    const CollisionDemo = new Demo();
+    CollisionDemo.addCircle(p);
+    // CollisionDemo.addCircle(o);
+    CollisionDemo.addWall(w);
+    CollisionDemo.addWall(e);
+    // w.render();
+
+    const startDemo = () => {
+        let time = Date.now();
+        let dt = (initialTime - time) / 1000.0;
+
+        CollisionDemo.update(dt);
+        CollisionDemo.render();
+
+        initialTime = time;
+        requestAnimationFrame(startDemo);
+    }
+
+    startDemo();
 });
+
+class Demo {
+    constructor() {
+        this.cirlcles = [];
+        this.walls = [];
+    }
+
+    addWall(wall) {
+        this.walls.push(wall);
+    }
+
+    addCircle(circle) {
+        this.cirlcles.push(circle);
+    }
+
+    update(dt) {
+        this.cirlcles.forEach(c => c.update(dt, this.walls));
+    }
+
+    render() {
+        ctx.clearRect(0, 0, c.width, c.height);
+        this.walls.forEach(wall => wall.render());
+        this.cirlcles.forEach(circle => circle.render());
+    }
+}
 
 class Point {
     constructor(x, y) {
@@ -42,22 +90,24 @@ class Circle {
         this.position = pos;
         this.radius = size;
 
+        this.speed = 50;
+
         this.velocity = {
             x: 0,
-            y: 50
+            y: this.speed
         };
     }
 
     checkCollision(wall) {
         const dist = (Math.abs(wall.a * this.position.x + wall.b * this.position.y + wall.c)) /
             Math.sqrt(wall.a * wall.a + wall.b * wall.b);
-        if (this.radius === dist) console.log('Touch');
-        else if (this.radius > dist) console.log('COLLISION');
+        if (this.radius - dist === .15) { debugger; console.log('Touch'); }
+        else if (this.radius > dist) { this.velocity.y *= -1; console.log('COLLISION'); }
         else console.log('safe');
     }
 
     update(dt, walls) {
-        this.position.y += this.velocity.y * dt;
+        this.position.y -= this.velocity.y * dt;
         walls.forEach(wall => this.checkCollision(wall));
     }
 
@@ -69,3 +119,29 @@ class Circle {
         ctx.fill();
     }
 }
+
+const lineToLineCollision = (wall1, wall2) => {
+    const a1 = wall1.p2.y - wall1.p1.y;
+    const b1 = wall1.p1.x - wall1.p2.x;
+    const c1 = a1 * wall1.p1.x + b1 * wall1.p1.y;
+
+    const a2 = wall2.p2.y - wall2.p1.y;
+    const b2 = wall2.p1.x - wall2.p2.x;
+    const c2 = a2 * wall2.p1.x + b2 * wall2.p1.y;
+
+    const det = a1 * b2 - a2 * b1;
+
+    if (det !== 0) {
+        const x = (b2 * c1 - b1 * c2) / det;
+        const y = (a1 * c2 - a2 * c1) / det;
+
+        if (x >= Math.min(wall1.p1.x, wall1.p2.x) && x <= Math.max(wall1.p1.x, wall1.p2.x) &&
+            x >= Math.min(wall2.p1.x, wall2.p2.x) && x <= Math.max(wall2.p1.x, wall2.p2.x) &&
+            y >= Math.min(wall1.p1.y, wall1.p2.y) && y <= Math.max(wall1.p1.y, wall1.p2.y) &&
+            y >= Math.min(wall2.p1.y, wall2.p2.y) && y <= Math.max(wall2.p1.y, wall2.p2.y)) {
+            return new Point(x, y);
+        }
+    }
+    return null;
+}
+
